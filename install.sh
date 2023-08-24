689adeee58d50cff76a3eea00a73bf8b0c8141c6
Jin Li <lijin.abc@gmail.com>
2023-08-24 04:09:12 -0500

    install.sh: fix installation steps

--- install.sh
+++ install.sh
@@ -4,17 +4,34 @@
 source trapdebug
 set -euo pipefail
 
-if [[ $MAMBA_ROOT_PREFIX ]]
-then
-	source "$MAMBA_ROOT_PREFIX/etc/profile.d/micromamba.sh"
-	micromamba activate
-else
-	source "$(conda info --base)/etc/profile.d/conda.sh"
-	conda activate
-fi
-
-mamba create -n u_scgpt poetry wandb python=3.10 pytorch pytorch-cuda=12.1 scvi-tools scanpy pandas numba scikit-misc umap-learn leidenalg matplotlib packaging ninja numpy h5py rpy2 cxx-compiler torchtext transformers jupyter chex=0.1.7 -c p    ytorch-nightly -c nvidia
-micromamba activate u_scgpt
+source "$MAMBA_ROOT_PREFIX/etc/profile.d/micromamba.sh"
+pkg=(
+poetry
+wandb
+python=3.10
+pytorch
+pytorch-cuda=12.1
+scvi-tools
+scanpy
+pandas
+numba
+scikit-misc
+umap-learn
+leidenalg
+matplotlib
+packaging
+ninja
+numpy
+h5py
+rpy2
+cxx-compiler
+torchtext
+transformers
+jupyter
+chex=0.1.7
+)
+mamba create -n u_scgpt -y -c pytorch-nightly -c nvidia "${pkg[@]}"
+mamba activate u_scgpt
 pip install flash-attn==1.0.9 --no-build-isolation
 pip install scib
 pip install git+https://github.com/lijinbio/scGPT
